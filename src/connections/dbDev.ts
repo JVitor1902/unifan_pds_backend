5f232c7aa9d11c4b242d2136fea0ee30fc52f31b
JVitor1902 <tititojv47@gmail.com>
2024-11-04 21:14:43 -0300

    test

--- src/connections/dbDev.ts
+++ src/connections/dbDev.ts
@@ -1,12 +1,25 @@
 import { DataSource } from "typeorm";
 import { Task } from "../models/task";
 
+// export const DevDataSource = new DataSource({
+//     type: "postgres",
+//     host: "localhost",
+//     port: 5432,
+//     username: "postgres",
+//     password: "postgres",
+//     database: "backend",
+//     entities: [Task]
+// })
+
 export const DevDataSource = new DataSource({
     type: "postgres",
-    host: "localhost",
+    host: "dpg-cskl08u8ii6s73ft0k30-a.oregon-postgres.render.com",
     port: 5432,
-    username: "postgres",
-    password: "postgres",
-    database: "backend",
-    entities: [Task]
+    username: "unifan_pds_tils_user",
+    password: "IIEgh4gX7lUWjVWChKfjiMLXDPeBE9ov",
+    database: "unifan_pds_tils",
+    entities: [Task],
+    extra: {
+        options: "-c statement_timeout=30000ms -c search_parth=monsters",
+    },
 })
